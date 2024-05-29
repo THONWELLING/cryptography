d552b3d2198e6339d84475606d99aea7404bbb55
Thonwelling <thonwellingdani13@gmail.com>
2024-05-28 23:36:20 -0300

    Definning the Encryption And Decryption Rules Into The Entity

--- src/main/java/br/com/ambarx/criptografia/entity/TransactionEntity.java
+++ src/main/java/br/com/ambarx/criptografia/entity/TransactionEntity.java
@@ -1,5 +1,6 @@
 package br.com.ambarx.criptografia.entity;
 
+import br.com.ambarx.criptografia.service.CryptoService;
 import jakarta.persistence.*;
 
 @Entity
@@ -70,4 +71,28 @@ public class TransactionEntity {
   public void setRawCreditCardToken(String rawCreditCardToken) {
     this.rawCreditCardToken = rawCreditCardToken;
   }
+
+
+  /**A Anotação @PrePersist quer dizer básicamente antes de persistir o(s) dado(s) no banco faça o seguinte:
+   * Nesse caso eu vou pegar os dados cru dos campos rawUserDocument e rawCreditCardToken e encryptar usando o CryptoService.encrypt()
+   * Isso vai encryptar os dados de texto comum e salvar no campo que vai representar realmente o dado na coluna do DB */
+  @PrePersist
+  public void prepPersist(){
+    this.encryptedUserDocument    = CryptoService.encrypt(rawUserDocument);
+    this.encryptedCreditCardToken = CryptoService.encrypt(rawCreditCardToken);
+  }
+
+  /**A Anotação @PosLoad quer dizer justo o contrário `quando os dados forem carregados do DB faça`
+   * Neste caso eu vou pegar o s dados encryptados dos campos encryptedUserDocument e encryptedCreditCardToken e
+   * desencryptar usando CryptoService.decrypt().
+   * Isso vai mostrar os dados em sua forma original(Text) nos campos rawUserDocument e rawCreditCardToken para que possam ser exibidos ao usuário
+   * */
+  @PostLoad
+  public void postLoad(){
+    this.rawUserDocument    = CryptoService.encrypt(encryptedUserDocument);
+    this.rawCreditCardToken = CryptoService.encrypt(encryptedCreditCardToken);
+  }
+
+
 }
+
