3d6d8917d274c47449928bc375844c149dc1ec28
Thonwelling <thonwellingdani13@gmail.com>
2024-05-28 22:52:34 -0300

    Creating Encryption And Decryption Service

--- src/main/java/br/com/ambarx/criptografia/service/CryptoService.java
+++ src/main/java/br/com/ambarx/criptografia/service/CryptoService.java
@@ -1,5 +1,31 @@
 package br.com.ambarx.criptografia.service;
 
-public class CryptoService {
+import org.jasypt.util.text.StrongTextEncryptor;
 
+public class CryptoService {
+//  public static void main(String[] args) {
+//    var encryptor = new StrongTextEncryptor();
+//    encryptor.setPassword("Thondev");
+//
+//    var encryptedText = encryptor.encrypt("ThonWelling The Best Dev I've ever saw!!");
+//
+//    var decryptedText = encryptor.decrypt(encryptedText);
+//
+//    System.out.println("O Texto Encryptado é: " + encryptedText);
+//    System.out.println("O Texto Decryptado é: " + decryptedText);
+//  }
+
+  private static final StrongTextEncryptor encryptor;
+  static {
+    encryptor = new StrongTextEncryptor();
+    encryptor.setPassword(System.getenv("APP_KEY"));
+  }
+
+  public static String encrypt(String rawText){
+    return encryptor.encrypt(rawText);
+  }
+
+  public static String decrypt(String encryptedText){
+    return encryptor.decrypt(encryptedText);
+  }
 }
